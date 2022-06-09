38ca650f2edd13252257c7d61681cdcd6b940332
haninemerzougui <merzouguihanine4@gmail.com>
2022-06-09 14:52:30 +0100

    test Commit

--- mainbk.cpp
+++ mainbk.cpp
@@ -8,7 +8,7 @@ int main(){
     Matrice * mat = new Matrice("base.txt");
     //print the first element of the matrix
     cout << mat->getValue(0,0) << endl;
-    //print the last element of the matrix
+    //print the last element of the matrix1235
     cout << mat->getValue(mat->getH()-1, mat->getW()-1) << endl;
     //print all the elements of the matrix
     for(int i = 0; i < mat->getH(); i++){
